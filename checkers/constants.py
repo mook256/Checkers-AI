753df394871ca592911580f4a234a9e7ac45c603
Kunanya <kununyasri2545@gmail.com>
2024-06-27 16:39:36 +0700

    all Added files written about the Checkers-AI project

---
+++ checkers/constants.py
@@ -0,0 +1,16 @@
+import pygame
+
+WIDTH, HEIGHT = 800, 800
+ROWS, COLS = 8, 8
+
+SQUARE_SIZE = WIDTH//COLS
+
+# rgb
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+Brown = (186, 112, 79)
+Beige = (223, 168, 120)
+
+CROWN = pygame.transform.scale(pygame.image.load('assets/crown.png'), (44, 25))
